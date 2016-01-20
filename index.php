ec338015bbcc18b82252436edfd716da44524ca0
Johanes Surya <johanes.surya43@gmail.com>
2016-01-21 00:12:07 +0700

    Rename file index.html to index.php

--- index.php
+++ index.php
@@ -21,10 +21,9 @@
 				<div class="inner">
 					<nav>
 						<ul>
-							<li><a href="#intro">Welcome</a></li>
-							<li><a href="#one">Who we are</a></li>
-							<li><a href="#two">What we do</a></li>
-							<li><a href="#three">Get in touch</a></li>
+							<li><a href="#intro">Home</a></li>
+							<li><a href="#one">Articles</a></li>
+							<li><a href="#three">Contact Us</a></li>
 						</ul>
 					</nav>
 				</div>
